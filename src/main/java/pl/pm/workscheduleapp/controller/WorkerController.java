98ef2b5663214b55071c40f139ac5b05473054d8
Patrycja Matusik <pmatusik11@gmail.com>
2018-11-22 18:50:43 +0100

    Displaying single worker

--- src/main/java/pl/pm/workscheduleapp/controller/WorkerController.java
+++ src/main/java/pl/pm/workscheduleapp/controller/WorkerController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.pm.workscheduleapp.model.Worker;
 import pl.pm.workscheduleapp.repository.WorkerRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class WorkerController {
@@ -37,5 +39,12 @@ public class WorkerController {
         List<Worker> allWorkers = workerRepository.findAll();
         model.addAttribute("workers", allWorkers);
         return "allWorkers";
+    }
+
+    @GetMapping("/worker/{surname}/{name}")
+    public String getWorker(Model model, @PathVariable("surname") String surname,@PathVariable("name") String name){
+        Optional<Worker> workerBySurnameAndName = workerRepository.findBySurnameAndNameIgnoreCase(surname, name);
+        workerBySurnameAndName.ifPresent(worker -> model.addAttribute("worker", worker));
+        return workerBySurnameAndName.map(worker -> "singleWorker").orElse("noWorker");
     }
 }
--- src/main/java/pl/pm/workscheduleapp/repository/WorkerRepository.java
+++ src/main/java/pl/pm/workscheduleapp/repository/WorkerRepository.java
@@ -3,6 +3,8 @@ package pl.pm.workscheduleapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.pm.workscheduleapp.model.Worker;
 
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
+import java.util.Optional;
 
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+    Optional<Worker> findBySurnameAndNameIgnoreCase(String surname, String name);
 }
