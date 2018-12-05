df671fa255620705a80449745b9dfbf95c84a553
Patrycja Matusik <pmatusik11@gmail.com>
2018-12-05 23:54:54 +0100

    Front page done

--- src/main/resources/layout/javascript/autosubmit.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var input = $("#auto_submit_item");
-    input.keyup(function () {
-        $(this).parents("#form").submit();
-    });
-    var len = input.val().length;
-    input[0].focus();
-    input[0].setSelectionRange(len, len);
-});
---
+++ src/main/resources/static/background.js
@@ -0,0 +1,1 @@
+$.backstretch("/img/68.jpg");
