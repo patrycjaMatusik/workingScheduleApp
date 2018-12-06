7ea5a3a27af7489f981b464282db3397b1771c2e
Patrycja Matusik <pmatusik11@gmail.com>
2018-12-06 10:24:13 +0100

    Looks pretty

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 insert into workers(name, surname, address, phone_number) values
 ('Jan', 'Kowalski', 'ul. Sosnowa 55/16, 75-896 Poznań', '987586951'),
-('Anna', 'Nowak', 'ul. Filipowa 12/36, 55-256 Wrocław', '+48548965745');
+('Anna', 'Nowak', 'ul. Filipowa 12/36, 55-256 Wrocław', '+48548965745'),
+('Alicja', 'Kolka', 'ul. Morska 15, 58-965 Myszkowice', '452658956');
 
 insert into schedules(work_date, start_working_hour, end_working_hour) values
 ('2018-11-26', '09:00', '17:00'),
